e53814dd316c012f5c0c05171ccfc4ae899cf296
Christopher Rybicki <crybicki98@gmail.com>
2022-07-25 12:05:57 +0300

    remove commented out options

--- .projenrc.js
+++ .projenrc.js
@@ -19,10 +19,5 @@ const project = new cdk.JsiiProject({
   githubOptions: {
     projenCredentials: github.GithubCredentials.fromApp(),
   },
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
 });
 project.synth();
