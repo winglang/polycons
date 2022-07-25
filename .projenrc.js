b24bdf239eb62530ed2aa9d927f3fb59d35cdda4
Christopher Rybicki <crybicki98@gmail.com>
2022-07-25 11:56:49 +0300

    copy projen config from wingsdk repo

--- .projenrc.js
+++ .projenrc.js
@@ -1,10 +1,24 @@
-const { cdk } = require('projen');
+const { cdk, github } = require("projen");
 const project = new cdk.JsiiProject({
-  author: 'Christopher Rybicki',
-  authorAddress: 'crybicki98@gmail.com',
-  defaultReleaseBranch: 'main',
-  name: 'polycons',
-  repositoryUrl: 'https://github.com/monadahq/polycons',
+  name: "polycons",
+  packageName: "@monadahq/polycons",
+  author: "Monada, Inc.",
+  authorAddress: "ping@monada.co",
+  defaultReleaseBranch: "main",
+  repositoryUrl: "https://github.com/monadahq/polycons",
+
+  peerDeps: ["constructs@^10"],
+  prettier: true,
+
+  npmRegistryUrl: "https://npm.pkg.github.com",
+  autoApproveUpgrades: true,
+  autoApproveOptions: {
+    allowedUsernames: ["monada-bot[bot]"],
+    secret: "PROJEN_GITHUB_TOKEN",
+  },
+  githubOptions: {
+    projenCredentials: github.GithubCredentials.fromApp(),
+  },
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
---
+++ API.md
@@ -0,0 +1,39 @@
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="Classes"></a>
+
+### Hello <a name="Hello" id="@monadahq/polycons.Hello"></a>
+
+#### Initializers <a name="Initializers" id="@monadahq/polycons.Hello.Initializer"></a>
+
+```typescript
+import { Hello } from '@monadahq/polycons'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="Methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| <code><a href="#@monadahq/polycons.Hello.sayHello">sayHello</a></code> | *No description.* |
+
+---
+
+##### `sayHello` <a name="sayHello" id="@monadahq/polycons.Hello.sayHello"></a>
+
+```typescript
+public sayHello(): string
+```
+
+
+
+
+
