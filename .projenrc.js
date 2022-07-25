8a30a8fcee326041038c98780e8cb79e206e793c
Christopher Rybicki <crybicki98@gmail.com>
2022-07-25 13:35:52 +0300

    feat: scaffolding project with projen (#4)

---
+++ .projenrc.js
@@ -0,0 +1,27 @@
+const { cdk, github } = require("projen");
+const project = new cdk.JsiiProject({
+  name: "polycons",
+  packageName: "@monadahq/polycons",
+  author: "Monada, Inc.",
+  authorOrganization: true,
+  authorAddress: "ping@monada.co",
+  defaultReleaseBranch: "main",
+  repositoryUrl: "https://github.com/monadahq/polycons",
+
+  peerDeps: ["constructs@^10"],
+  prettier: true,
+
+  npmRegistryUrl: "https://npm.pkg.github.com",
+  autoApproveUpgrades: true,
+  autoApproveOptions: {
+    allowedUsernames: ["monada-bot[bot]"],
+    secret: "PROJEN_GITHUB_TOKEN",
+  },
+  githubOptions: {
+    projenCredentials: github.GithubCredentials.fromApp(),
+  },
+
+  codeCov: true,
+  codeCovTokenSecret: "CODECOV_TOKEN",
+});
+project.synth();
