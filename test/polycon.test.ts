8c59b9cbd4fb9499f257c6e5895205af9e1eceae
Christopher Rybicki <chrisr@monada.co>
2022-07-26 19:42:55 -0400

    feat: this inheritance hack can't possibly work can it?

--- test/polycon.test.ts
+++ test/polycon.test.ts
@@ -4,7 +4,7 @@ import { Polycon, PolyconFactory } from "../src";
 test("Polycon.isPolycon returns true for polycons", () => {
   const app = new App();
   PolyconFactory.register(app, new PoodleFactory());
-  const dog = new Dog(app, "dog", { treats: 5 });
+  const dog = new Dog(app, "dog", { name: "piffle", treats: 5 });
 
   expect(Polycon.isPolycon(dog)).toBeTruthy();
   expect(Polycon.isPolycon(app)).toBeFalsy();
@@ -45,7 +45,7 @@ test("you cannot register two polycon factories to a single tree", () => {
 
 test("a polycon cannot be instantiated without a factory", () => {
   const app = new App();
-  expect(() => new Dog(app, "dog", { treats: 5 })).toThrowError(
+  expect(() => new Dog(app, "dog", { name: "piffle", treats: 5 })).toThrowError(
     /No polycon factory has been registered to the construct tree/
   );
 });
@@ -61,14 +61,25 @@ test("a polycon cannot be instantiated if the registered factory does not suppor
 test("polycons can be resolved to constructs by a factory", () => {
   const app = new App();
   PolyconFactory.register(app, new PoodleFactory());
-  const piffle = new Dog(app, "piffle", { treats: 5 });
-  const biffle = new Dog(app, "biffle", { treats: 7 });
+  const piffle = new Dog(app, "piffle", { name: "piffle", treats: 5 });
+  const biffle = new Dog(app, "biffle", { name: "biffle", treats: 7 });
 
   expect(piffle.toString()).toEqual("Poodle with 5 treats.");
   expect(biffle.toString()).toEqual("Poodle with 7 treats.");
 
   expect(app.synth()).toEqual(["root", "root/piffle", "root/biffle"]);
 });
+
+test("polycons can access properties and methods from their parent classes", () => {
+  const app = new App();
+  PolyconFactory.register(app, new PoodleFactory());
+  const piffle = new Dog(app, "piffle", { name: "piffle", treats: 5 });
+
+  // method is defined on Dog but available to Poodle via inheritance
+  expect(piffle.toStringUppercase()).toEqual("POODLE WITH 5 TREATS.");
+  // property is defined on Dog but available to Poodle via inheritance
+  expect(piffle.species).toEqual("Canis familiaris");
+})
 
 class App extends Construct {
   constructor() {
@@ -84,22 +95,27 @@ class App extends Construct {
 const DOG_QUALIFIER = "test.dog";
 
 interface DogProps {
+  readonly name: string;
   readonly treats: number;
 }
 
 class Dog extends Polycon {
+  public readonly species = "Canis familiaris";
   constructor(scope: Construct, id: string, props: DogProps) {
     super(DOG_QUALIFIER, scope, id, props);
   }
-  public toString() {
+  public toStringUppercase() {
+    return this.toString().toUpperCase();
+  }
+  public toString(): string {
     throw new Error("unimplemented");
   }
 }
 
-class Poodle extends Construct {
+class Poodle extends Dog {
   public readonly treats: number;
   constructor(scope: Construct, id: string, props: DogProps) {
-    super(scope, id);
+    super(scope, id, props);
     this.treats = props.treats;
   }
   public toString() {
@@ -119,7 +135,7 @@ class Cat extends Polycon {
   constructor(scope: Construct, id: string, props: CatProps) {
     super(CAT_QUALIFIER, scope, id, props);
   }
-  public toString() {
+  public toString(): string {
     throw new Error("unimplemented");
   }
 }
